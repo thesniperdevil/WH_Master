565e77e2934a6feb8419beadcb20cd5454dca2c2
thesniperdevil <39886228+thesniperdevil@users.noreply.github.com>
2018-12-09 18:20:40 +0000

    -fixing random edits.

--- .vscode/custom_types.lua
+++ .vscode/custom_types.lua
@@ -1,7 +1,3 @@
 --# assume global class VRM
-<<<<<<< HEAD
 --# assume global class EO  --for the event object for LL lore
 --# assume global class FRM -- region manager
-=======
---# assume global class EO
->>>>>>> c8c77fa2969433971bcec1d1d5f0446e521884c7
