bc0d8128a0765b3d70ec0bb1ca3d21ae6f0e3d04
thesniperdevil <39886228+thesniperdevil@users.noreply.github.com>
2018-12-09 18:21:18 +0000

    fixing random issues

--- .vscode/entry.lua
+++ .vscode/entry.lua
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 require("experiments/paste_dump")
 require("experiments/region_politics")
-=======
---require("experiments/paste_dump")
 require("Legendary Lord Lore/tp_ll_lore")
->>>>>>> c8c77fa2969433971bcec1d1d5f0446e521884c7
---require("stec_project/stec_archive/vassal_regions")
